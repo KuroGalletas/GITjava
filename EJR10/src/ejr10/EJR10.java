65e887ad5cbb41cc5b0e8934693ddd92e066884d
KuroGalletas <75172807+KuroGalletas@users.noreply.github.com>
2022-10-03 01:48:47 -0500

    EJR10

---
+++ EJR10/src/ejr10/EJR10.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejr10;
+
+/**
+ *
+ * @author ACER
+ */
+public class EJR10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ventana2 miventana2 = new ventana2 ();
+        miventana2.show(true);
+        ventana miventana = new ventana ();
+        miventana.show(false);
+    }
+    
+}
